efadfabb6ce55f1d40c3d6f43b1d3291fe708b75
CsMargareta <72508599+csorema@users.noreply.github.com>
2023-12-16 19:30:18 +0100

    Delete Webtechfeladat directory

--- Webtechfeladat/Webtechfeladat/fadeInAnimation.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Az oldal tartalmának kiválasztása
-    var mainContent = document.getElementById('orokbefogadas');
-  
-    // Hozzáadja az animációs osztályt az oldal tartalmához
-    mainContent.classList.add('fade-in');
-  });
